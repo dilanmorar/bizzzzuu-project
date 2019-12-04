d18120cc8ed3deb86051557a61ebec6b8f23663e
Dilan Morar <DMorar@spartaglobal.com>
2019-12-04 13:51:53 +0000

    start of code, no loops

--- bizzzzuu_game.py
+++ bizzzzuu_game.py
@@ -1,3 +1,14 @@
 from bizzzzuu_functions import *
- grbgtgtg
- vfmklfmvbl
+
+number = int(input('Choose a number: '))
+if div_by_3_and_5(number):
+    print('bizzzzuu')
+elif div_by_5(number):
+    print('zzuu')
+elif div_by_3():
+    print('bizz')
+elif number == 'exit':
+    print('exited')
+    break
+else:
+    print(number)
