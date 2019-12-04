f1787e6af8f7c1275c201ece7e0fe1eaa1bf1731
Dilan Morar <DMorar@spartaglobal.com>
2019-12-04 15:52:38 +0000

    works with numbers but not when I try and exit

--- bizzzzuu_functions.py
+++ bizzzzuu_functions.py
@@ -1,10 +1,10 @@
 # defining the fuctions
 
-def div_by_3(number):
-    return number%3 == 0
+def div_by_3(arg1):
+    return arg1%3 == 0
 
-def div_by_5(number):
-    return number%5 == 0
+def div_by_5(arg1):
+    return arg1%5 == 0
 
-def div_by_3_and_5(number):
-    return number%3 == 0 and number%5 == 0
+def div_by_3_and_5(arg1):
+    return arg1%3 == 0 and arg1%5 == 0
--- bizzzzuu_game.py
+++ bizzzzuu_game.py
@@ -1,14 +1,20 @@
 from bizzzzuu_functions import *
 
-number = int(input('Choose a number: '))
-if div_by_3_and_5(number):
-    print('bizzzzuu')
-elif div_by_5(number):
-    print('zzuu')
-elif div_by_3():
-    print('bizz')
-elif number == 'exit':
-    print('exited')
-    break
-else:
-    print(number)
+number = input('Choose a number: ')
+
+while True:
+    if div_by_3_and_5(int(number)):
+        print('bizzzzuu')
+        number = input('Choose a number: ')
+    elif div_by_5(int(number)):
+        print('zzuu')
+        number = input('Choose a number: ')
+    elif div_by_3(int(number)):
+        print('bizz')
+        number = input('Choose a number: ')
+    elif number == 'exit':
+        print('exited')
+        break
+    else:
+        print(int(number))
+        number = input('Choose a number: ')
